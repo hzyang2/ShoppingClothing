854f54ec2892845254bba7fb526e40e451e86c4b
Luke Yang <hzyang2@gmail.com>
2020-08-19 11:42:14 -0500

    initial commit

---
+++ src/main/java/org/launchcode/shoppingclothing/ShoppingclothingApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.shoppingclothing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingclothingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingclothingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/shoppingclothing/ShoppingclothingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.shoppingclothing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingclothingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
