5e210c28dc91956c2393bdaf0780107c4de5a8ff
Luke Yang <hzyang2@gmail.com>
2020-11-07 14:18:40 -0600

    recovered removed coding

--- src/main/java/org/launchcode/shoppingclothing/models/ShippingAndPayment.java
+++ src/main/java/org/launchcode/shoppingclothing/models/ShippingAndPayment.java
@@ -55,4 +55,92 @@ public class ShippingAndPayment {
     private String cardverificationnumber;
 
     public ShippingAndPayment() {}
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getStreetaddress() {
+        return streetaddress;
+    }
+
+    public void setStreetaddress(String streetaddress) {
+        this.streetaddress = streetaddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getCreditcardnumber() {
+        return creditcardnumber;
+    }
+
+    public void setCreditcardnumber(String creditcardnumber) {
+        this.creditcardnumber = creditcardnumber;
+    }
+
+    public String getCardverificationnumber() {
+        return cardverificationnumber;
+    }
+
+    public void setCardverificationnumber(String cardverificationnumber) {
+        this.cardverificationnumber = cardverificationnumber;
+    }
+
+    public String getExpirationmonth() {
+        return expirationmonth;
+    }
+
+    public void setExpirationmonth(String expirationmonth) {
+        this.expirationmonth = expirationmonth;
+    }
+
+    public String getExpirationyear() {
+        return expirationyear;
+    }
+
+    public void setExpirationyear(String expirationyear) {
+        this.expirationyear = expirationyear;
+    }
 }
